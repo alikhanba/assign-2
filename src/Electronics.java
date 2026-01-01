043344754a7ae8dd87e394c5c585ec3a0cf64795
keons <fsomething180@proton.me>
2026-01-01 21:47:31 +0500

    created subclass for the product

---
+++ src/Electronics.java
@@ -0,0 +1,17 @@
+public class Electronics extends Product {
+ private String brand;
+ public Electronics(String name, double price, int quantity, String brand) {
+     super(name, price, quantity);
+     this.brand = brand;
+ }
+ public String getBrand() {
+     return brand;
+ }
+ public void setBrand(String brand){
+     this.brand=brand;
+ }
+ @Override
+    public String toString() {
+     return super.toString()+ "[Brand: " + brand + "]";
+ }
+}
--- src/Product.java
+++ src/Product.java
@@ -1,5 +1,4 @@
 import java.util.Objects;
-
 public class Product{
     private String name;
     private double price;
